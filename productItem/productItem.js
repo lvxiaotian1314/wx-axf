1e9c03186c62c95b83d0fc6e4239e3815340498f
lvxiaotian1314 <lvxinbo666@163.com>
2018-02-05 16:54:05 +0800

    商品详情页静态页面，tabBar页面背景图base64转图床短链接

--- productItem/productItem.js
+++ productItem/productItem.js
@@ -1,43 +1,27 @@
-// pages/home/home.js
-// let api=require('/utils/api.js')
+var api = require("../utils/api.js")
+//productItem.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-    ],
-    indicatorDots: true,
-    autoplay: true,
-    interval: 5000,
-    duration: 1000,
-    categories:{},
+    product:{}
   },
-  changeIndicatorDots: function (e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    let app = getApp()
-    let api=require("../../utils/api.js")
-    this.setData({
-      categories:app.globalData.categories
-    })
+  onLoad: function (option) {
+    let id=option.id
     wx.request({
-      url: api.host+"/bannar",
+      url: api.host+"/products?id="+id,
       success:res=>{
-        let imgUrls=[]
-        for(let i=0;i<res.data.length;i++){
-          imgUrls.push(res.data[i].bannar_img)
-        }
+        console.info(res.data)
         this.setData({
-          imgUrls:imgUrls
+          product:res.data[0]
         })
+        console.log(this.data.product.product_name)
       }
     })
   },
@@ -46,57 +30,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  },
-  // 跳转商品详情页
-  goProductitem(event){
-    // 商品id
-    let id=event.currentTarget.dataset.id
-    wx.navigateTo({
-      url: '/productItem/productItem?id=' + id
-    })
-    
+
   }
 })
